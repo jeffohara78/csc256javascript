fb8c548a61c5868dc0b05765f816a3cf7e1b6d4b
jeffohara78 <85190271+jeffohara78@users.noreply.github.com>
2023-01-19 14:55:41 -0700

    Add files via upload

--- helper.js
+++ helper.js
@@ -1,3 +1,5 @@
+
+// Create a function to display additional text to the screen for the user
 function displayText(){
         document.getElementById("myText").innerHTML = "A quick introduction to myself: I am a father of three boys, two puppies, and a husband to a beautiful wife. I'm originally from the New England area, but call the desert my home now.";
 }
